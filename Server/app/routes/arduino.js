9df6532e4ba860b33ae2df2bf66916fa995ec143
Ricardo T. Ono <ricardotadashiono@gmail.com>
2017-12-11 21:51:22 -0200

    ...

--- Server/app/routes/arduino.js
+++ Server/app/routes/arduino.js
@@ -73,15 +73,5 @@ module.exports = function (app) {
             }
             res.send('bateu').end();
         });
-        var rgb = new five.Led.RGB([6, 5, 3]);
-        var index = 0;
-        var rainbow = ["FF0000", "FF7F00", "FFFF00", "00FF00", "0000FF", "4B0082", "8F00FF"];
-
-        this.loop(1000, function() {
-            rgb.color(rainbow[index++]);
-            if (index === rainbow.length) {
-                index = 0;
-            }
-        });
     });
 };
